3622af471951ac5e00131cda8b084db1b337de4c
Pontus Kronenberg <a21ponkr@student.his.se>
2022-05-29 20:23:55 +0200

    help list, and some info to a few of the commands

---
+++ js/cmd.js
@@ -0,0 +1,28 @@
+help = [
+    "<br>",
+    '<span class="cmd">whoami</span>          Who am I?',
+    '<span class="cmd">projects</span>          See what projects I have worked on',
+    '<span class="cmd">clear</span>          clear command line',
+    '<span class="cmd">socials</span>          My socials',
+    '<span class="cmd">help</span>          help command',
+    "<br>",
+];
+
+whoami = [
+    "<br>",
+    "Hello! My name is Pontus.",
+    "And this is my website.",
+    "<br>"
+];
+
+projects = [
+      "<br>",
+      "N/A",
+      "<br>"
+];
+
+socials = [
+    "<br>",
+    'github         <a href="' + github + '" target="_blank">github/a21ponkr' + "</a>",
+    "<br>"
+];
